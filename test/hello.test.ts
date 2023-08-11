97c32795a068086b5aa0ca0d9f10a4e469f076ac
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-11 19:15:45 +0900

    hello.test.ts: commit for now

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,23 +1,50 @@
-import { of } from "rxjs";
-import { map } from "rxjs/operators";
+import { lastValueFrom, of } from "rxjs";
+import { filter, map, reduce, toArray } from "rxjs/operators";
 
-export const doubleNumbers = () => {
-  return of(1, 2, 3, 4, 5).pipe(map((num) => num * 2));
+// Double the value of each element in the array.
+const double1 = (...values: number[]) => {
+  return of(...values).pipe(
+    map((num) => num * 2),
+    toArray()
+  );
+};
+it("double1", async () => {
+  const observable = double1(1, 2, 3, 4, 5);
+  const result = await lastValueFrom(observable);
+  expect(result).toEqual([2, 4, 6, 8, 10]);
+});
+
+// An example of extracting values one by one from a stream.
+const double2 = (...values: number[]) => {
+  return of(...values).pipe(map((num) => num * 2));
+};
+it("double2", (done) => {
+  const expected = [2, 4, 6, 8, 10];
+  let index = 0;
+
+  double2(1, 2, 3, 4, 5).subscribe({
+    next: (val) => {
+      expect(val).toEqual(expected[index]);
+      index++;
+    },
+    complete: () => {
+      done();
+    },
+  });
+});
+
+// Extract only the odd numbers from the array elements,
+// and return the sum of the squares of each element
+const oddSquare = (...values: number[]) => {
+  return of(...values).pipe(
+    filter((num) => num % 2 !== 0),
+    map((num) => num * num),
+    reduce((acc, val) => acc + val, 0)
+  );
 };
 
-describe("doubleNumbers", () => {
-  it("should double each number", (done) => {
-    const expected = [2, 4, 6, 8, 10];
-    let index = 0;
-
-    doubleNumbers().subscribe({
-      next: (val) => {
-        expect(val).toEqual(expected[index]);
-        index++;
-      },
-      complete: () => {
-        done();
-      },
-    });
-  });
+it("oddSquare", async () => {
+  const ob = oddSquare(1, 2, 3, 4, 5);
+  const result = await lastValueFrom(ob);
+  expect(result).toEqual(35);
 });

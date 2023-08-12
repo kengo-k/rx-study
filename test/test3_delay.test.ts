8c374c1d1d65e57dde50313bc316ffb8fe299669
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-12 16:50:41 +0900

    fix: Modify to ensure the async process completes

--- test/test3_delay.test.ts
+++ test/test3_delay.test.ts
@@ -1,34 +1,56 @@
 import { from, lastValueFrom, merge, of } from "rxjs";
 import { concatAll, concatMap, delay, map, toArray } from "rxjs/operators";
 
-it("print the all values 3 seconds after it arrives in the stream", () => {
-  of(1, 2, 3, 4, 5).pipe(delay(3000)).subscribe(console.log);
+it("print the all values 3 seconds after it arrives in the stream", (done) => {
+  of(1, 2, 3, 4, 5)
+    .pipe(delay(500))
+    .subscribe({
+      next: console.log,
+      complete: () => {
+        done();
+      },
+    });
 });
 
-it("print the values arriving in the stream at 100 msec intervals", () => {
+it("print the values arriving in the stream at 100 msec intervals", (done) => {
   const create = (val: number) => {
     return of(val).pipe(delay(100));
   };
   const stream$ = of(create(1), create(2), create(3), create(4), create(5));
   const concatStream$ = stream$.pipe(concatAll());
-  concatStream$.subscribe(console.log);
+  concatStream$.subscribe({
+    next: console.log,
+    complete: () => {
+      done();
+    },
+  });
 });
 
-it("simplify the above test using `map`", () => {
+it("simplify the above test using `map`", (done) => {
   const values = [1, 2, 3, 4, 5];
   const stream$ = from(values).pipe(
     map((val) => of(val).pipe(delay(100))),
     concatAll()
   );
-  stream$.subscribe(console.log);
+  stream$.subscribe({
+    next: console.log,
+    complete: () => {
+      done();
+    },
+  });
 });
 
-it("simplify the above test further using `concatMap`", () => {
+it("simplify the above test further using `concatMap`", (done) => {
   const values = [1, 2, 3, 4, 5];
   const stream$ = from(values).pipe(
     concatMap((val) => of(val).pipe(delay(100)))
   );
-  stream$.subscribe(console.log);
+  stream$.subscribe({
+    next: console.log,
+    complete: () => {
+      done();
+    },
+  });
 });
 
 it("merge two streams that receive data at different intervals.", async () => {

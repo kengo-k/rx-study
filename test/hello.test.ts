07b7fec17dbbc9830871ff26e983319ec7baccfa
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-12 11:40:11 +0900

    update: hello

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,50 +1,72 @@
 import { lastValueFrom, of } from "rxjs";
 import { filter, map, reduce, toArray } from "rxjs/operators";
 
-// Double the value of each element in the array.
-const double1 = (...values: number[]) => {
-  return of(...values).pipe(
-    map((num) => num * 2),
-    toArray()
-  );
+// Convert the values specified by the argument into a stream.
+const toStream = (...values: number[]) => {
+  return of(...values);
 };
-it("double1", async () => {
-  const observable = double1(1, 2, 3, 4, 5);
-  const result = await lastValueFrom(observable);
-  expect(result).toEqual([2, 4, 6, 8, 10]);
-});
 
-// An example of extracting values one by one from a stream.
-const double2 = (...values: number[]) => {
-  return of(...values).pipe(map((num) => num * 2));
-};
-it("double2", (done) => {
-  const expected = [2, 4, 6, 8, 10];
-  let index = 0;
-
-  double2(1, 2, 3, 4, 5).subscribe({
-    next: (val) => {
-      expect(val).toEqual(expected[index]);
-      index++;
-    },
+// Print the value that flowed into the stream to the console.
+// The argument passed to `subscribe` is called an Observer.
+// An Observer is an object that has a `next` method.
+// If passing a single function is equivalent to passing an Observer that only has a next method.
+it("subscribe to the stream", () => {
+  toStream(1, 2, 3, 4, 5).subscribe(console.log);
+  // subscribe to the stream with Observer object
+  toStream(1, 2, 3, 4, 5).subscribe({
+    next: console.log,
+    error: () => {},
     complete: () => {
-      done();
+      console.log("Complete!");
     },
   });
 });
 
-// Extract only the odd numbers from the array elements,
+it("verify the value of the stream using expect", () => {
+  const expected = [1, 2, 3, 4, 5];
+  let index = 0;
+  toStream(1, 2, 3, 4, 5).subscribe({
+    next: (val) => {
+      expect(val).toEqual(expected[index]);
+      index++;
+    },
+  });
+});
+
+it("convert the stream to an array and verify the entire array.", async () => {
+  const observable = toStream(1, 2, 3, 4, 5).pipe(toArray());
+  const result = await lastValueFrom(observable);
+  expect(result).toEqual([1, 2, 3, 4, 5]);
+});
+
+// Double the value that flowed into the stream.
+const double = (...values: number[]) => {
+  return toStream(...values).pipe(map((num) => num * 2));
+};
+
+it("double", async () => {
+  const observable = double(1, 2, 3, 4, 5).pipe(toArray());
+  const result = await lastValueFrom(observable);
+  const expected = [2, 4, 6, 8, 10];
+  expect(result).toEqual(expected);
+});
+
+// Extract only the odd numbers from the stream,
 // and return the sum of the squares of each element
 const oddSquare = (...values: number[]) => {
-  return of(...values).pipe(
-    filter((num) => num % 2 !== 0),
-    map((num) => num * num),
-    reduce((acc, val) => acc + val, 0)
+  return (
+    toStream(...values)
+      //You can split the call to pipe into multiple times.
+      .pipe(filter((num) => num % 2 !== 0))
+      .pipe(
+        map((num) => num * num),
+        reduce((acc, val) => acc + val, 0)
+      )
   );
 };
 
 it("oddSquare", async () => {
-  const ob = oddSquare(1, 2, 3, 4, 5);
+  const ob = oddSquare(1, 2, 3, 4, 5, 6);
   const result = await lastValueFrom(ob);
   expect(result).toEqual(35);
 });
